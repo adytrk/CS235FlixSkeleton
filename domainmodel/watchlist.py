6561c72e9ff96c437cd5dea08ccd0e24230e86e8
adytrk <your>
2020-09-03 20:19:15 -0700

    Extension 1

--- domainmodel/watchlist.py
+++ domainmodel/watchlist.py
@@ -1,4 +1,48 @@
 from domainmodel.movie import Movie
 
+
 class WatchList:
-    pass
+    def __init__(self):
+        self.__watchlist = []
+
+    @property
+    def watchlist(self):
+        return self.__watchlist;
+
+    def add_movie(self, movie : Movie):
+        if movie not in self.__watchlist:
+            self.__watchlist.append(movie)
+
+    def remove_movie(self, movie : Movie):
+        if movie in self.__watchlist:
+            self.__watchlist.remove(movie)
+
+    def select_movie_to_watch(self, index):
+        try:
+            return self.__watchlist[index]
+        except IndexError:
+            return None
+
+    def size(self):
+        return len(self.__watchlist)
+
+    def first_movie_in_watchlist(self):
+        if len(self.__watchlist) > 0:
+            return self.__watchlist[0]
+        return None
+
+    def __iter__(self):
+        self.__n = 0
+        return self
+
+    def __next__(self):
+        if self.__n <= self.size() - 1:
+            result = self.__watchlist[self.__n]
+            self.__n += 1
+            return result
+        else:
+            raise StopIteration
+
+
+
+
