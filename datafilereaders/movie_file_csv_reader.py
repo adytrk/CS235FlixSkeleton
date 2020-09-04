c5296768a562b583441f091b0e76e8cdcdb50c31
adytrk <your>
2020-09-04 00:51:01 -0700

    Extension 3

--- datafilereaders/movie_file_csv_reader.py
+++ datafilereaders/movie_file_csv_reader.py
@@ -54,20 +54,52 @@ class MovieFileCSVReader:
                     self.__dataset_of_directors.add(Director(director))
 
                 for genre in genres_list.split(','):
+                    movie.genres.append(Genre(genre))
                     if Genre(genre) not in self.__dataset_of_genres:
                         self.__dataset_of_genres.add(Genre(genre))
 
+
                 rank = row['Rank']
-                if rank > 0:
+                if int(rank) > 0:
                     movie.rank = rank
                 rating = row['Rating']
-                if  0 <= rating <= 10:
+                if  0 <= float(rating) <= 10:
                     movie.rating = rating
                 votes = row['Votes']
                 movie.votes = int(votes)
                 revenue_millions = row['Revenue (Millions)']
                 movie.revenue_millions = revenue_millions
                 metascore = row['Metascore']
-                if 0<= metascore <= 100:
+                try:
+                    if 0 <= float(metascore) <= 100:
+                        movie.metascore = metascore
+                except ValueError:
                     movie.metascore = metascore
+
+
+    def find_popular_genre(self):
+        genres = {}
+        for movie in self.__dataset_of_movies:
+            for genre in movie.genres:
+                if genre in genres:
+                    genres[genre] += 1
+                else:
+                    genres[genre] = 1
+        genres_list = sorted(genres.items(), key=lambda x: x[1], reverse=True)
+        return genres_list
+
+class TesMovieFileCSVReaderMethods:
+
+    def test_init(self):
+        filename = 'Data1000Movies.csv'
+        movie_file_reader = MovieFileCSVReader(filename)
+        movie_file_reader.read_csv_file()
+        movie_file_reader.find_popular_genre()
+        x = Genre("Drama")
+        assert movie_file_reader.find_popular_genre()[0] == (x, 513)
+
+        print(f'number of unique movies: {len(movie_file_reader.dataset_of_movies)}')
+        print(f'number of unique actors: {len(movie_file_reader.dataset_of_actors)}')
+        print(f'number of unique directors: {len(movie_file_reader.dataset_of_directors)}')
+        print(f'number of unique genres: {len(movie_file_reader.dataset_of_genres)}')
 
